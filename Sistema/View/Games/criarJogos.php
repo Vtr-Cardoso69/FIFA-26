a330020e3aa1472200469aeaf5daafaed8524f8a
WPOTC <gutomergr.s@gmail.com>
2026-02-13 15:34:57 -0300

    iniciei a estilização

--- Sistema/View/Games/criarJogos.php
+++ Sistema/View/Games/criarJogos.php
@@ -19,6 +19,7 @@ $gameController = new GameController($pdo);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FIFA-26</title>
+    <link rel="stylesheet" href="../../../css/criarJogos.css">
 </head>
 <body>
 
--- Sistema/View/Games/listarJogos.php
+++ Sistema/View/Games/listarJogos.php
@@ -7,9 +7,38 @@ require_once "C:/Turma2/xampp/htdocs/FIFA-26/Sistema/Controller/GameC.php";
 $gameController = new GameController($pdo);
 
 $jogos = $gameController->listarTodosJogos();
+?>
 
 
-echo "<div class='jogo'>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../../../css/listarJogos.css">
+</head>
+<body>
+<style>
+        :root {
+            --primary-color: #004d98; /* Azul FIFA */
+            --secondary-color: #ffffff;
+            --accent-color: #ed1c24; /* Vermelho FIFA */
+            --dark-bg: #0b0e14;
+        }
+</style>
+<header>
+  <h2>FIFA-<span style="color: var(--primary-color);">26</span></h2>
+  <nav class="nav">
+    <ul>
+      <li><a href="../../../index.php">Início</a></li>
+      <li><a href="../Teams/listarSelecoes.php">Seleções</a></li>
+      <li><a href="../Groups/listarGrupos.php">Grupos</a></li>
+      <li><a href="listarJogos.php">Jogos</a></li>
+    </ul>
+</header>
+
+<?php
 echo "<h1>Jogos</h1>";
 
 if(empty($jogos)) {
@@ -26,9 +55,9 @@ foreach ($jogos as $jogo) {
   $gols_mandante = $jogo['gols_mandante'];
   $gols_visitante = $jogo['gols_visitante'];
 
-
+echo "<div class='jogo'>";
 echo "<h2>Grupo: " . "$grupo" . "</h2> <br>";
-echo "<h3> " . $selecao_mandante . "vs" . $selecao_visitante . "</h3> <br>";
+echo "<h3> " . $selecao_mandante . " vs " . $selecao_visitante . "</h3> <br>";
 echo "Estádio: " . $estadio . "<br>";
 echo "Data e Hora: " . $data_hora . "<br>";
 echo "Fase: " . $fase . "<br>";
@@ -38,12 +67,26 @@ echo "Gols Mandante: " . $gols_mandante . "<br>";
 echo "Gols Visitante: " . $gols_visitante . "<br>";
 
 $gameController->classificacao($id, $gols_mandante, $gols_visitante, $selecao_mandante, $selecao_visitante);
-
 echo "</div>";
-
-
-
-
-} 
+echo "<br><br>";
+}
 
 ?>
+
+    <footer class="footer">
+        <div class="container">
+            <p>&copy; 2026 FIFA World Cup Project - Desenvolvido por FIFA DEVs</p>
+            <div class="">
+                <a href="https://github.com/WPOTC">Colaborador 1</a>
+                <a href="#">Colaborador 2</a>
+                <a href="">Colaborador 3</a>
+            </div>
+        </div>
+    </footer>
+
+
+</body>
+</html>
+
+
+
